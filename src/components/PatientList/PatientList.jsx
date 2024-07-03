9f955da2f823b8c43aa76eedbbdce77ba01dc552
Sergey Crew <sergeycrew.bs@gmail.com>
2024-07-03 10:13:07 -0700

    Patient list

--- src/components/PatientList/PatientList.jsx
+++ src/components/PatientList/PatientList.jsx
@@ -2,6 +2,7 @@ import patients from 'assets/patients.json';
 import sprite from 'assets/sprite.svg';
 import {
   Icon,
+  List,
   ListItem,
   Title,
   TitleWrapper,
@@ -18,19 +19,23 @@ export const PatientList = () => {
           <use href={`${sprite}#search_FILL0_wght300_GRAD0_opsz24`}></use>
         </Icon>
       </TitleWrapper>
-      {patients.map((patient) => (
-        <ListItem key={patient.name}>
-          <PersonData
-            src1x={patient.profile_picture_1x}
-            src2x={patient.profile_picture_2x}
-            name={patient.name}
-            title={`${patient.gender}, ${patient.age}`}
-          />
-          <Icon>
-            <use href={`${sprite}#more_horiz_FILL0_wght300_GRAD0_opsz24`}></use>
-          </Icon>
-        </ListItem>
-      ))}
+      <List>
+        {patients.map((patient) => (
+          <ListItem key={patient.name}>
+            <PersonData
+              src1x={patient.profile_picture_1x}
+              src2x={patient.profile_picture_2x}
+              name={patient.name}
+              title={`${patient.gender}, ${patient.age}`}
+            />
+            <Icon>
+              <use
+                href={`${sprite}#more_horiz_FILL0_wght300_GRAD0_opsz24`}
+              ></use>
+            </Icon>
+          </ListItem>
+        ))}
+      </List>
     </Wrapper>
   );
 };
--- src/components/PatientList/PatientList.styled.jsx
+++ src/components/PatientList/PatientList.styled.jsx
@@ -1,10 +1,11 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.div`
+  margin-top: 32px;
   background-color: ${({ theme }) => theme.colors.white};
   width: 367px;
-  height: 1054px;
-  padding: 20px;
+  height: 990px;
+  padding: 20px 4px 20px 20px;
   border-radius: 16px;
 `;
 
@@ -12,6 +13,8 @@ export const TitleWrapper = styled.div`
   display: flex;
   justify-content: space between;
   align-items: center;
+  margin-bottom: 40px;
+  padding-right: 18px;
 `;
 
 export const Title = styled.h2`
@@ -28,13 +31,36 @@ export const Icon = styled.svg`
 `;
 
 export const List = styled.ul`
-  //width: 367;
-  //display: flex;
-  //flex-direction: column;
+  overflow-y: scroll;
+  height: 900px;
+  padding-right: 30px;
+  &::-webkit-scrollbar {
+    width: 6px;
+  }
+
+  &::-webkit-scrollbar-track {
+    background: #f1f1f1;
+    border-radius: 3px;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background: #072635;
+    border-radius: 3px;
+    opacity: 1;
+  }
+
+  &::-webkit-scrollbar-thumb:hover {
+    background: #555;
+  }
+
+  /* Firefox */
+  scrollbar-width: thin;
+  scrollbar-color: #072635 #f1f1f1;
 `;
 
 export const ListItem = styled.li`
   display: flex;
   justify-content: space-between;
   align-items: center;
+  margin-bottom: 32px;
 `;
